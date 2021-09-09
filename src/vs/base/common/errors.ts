0f7b312a2b4b38169aad3ef0766fec31126f0d6c
Christian Butcher <chrisb2244@users.noreply.github.com>
2021-09-09 14:30:29 +0900

    Move quotation mark in error.ts
    readonly function quotes `'${name} cannot be changed'` when presumably `'${name}' cannot be changed` better matches expectation.

--- src/vs/base/common/errors.ts
+++ src/vs/base/common/errors.ts
@@ -180,7 +180,7 @@ export function illegalState(name?: string): Error {
 
 export function readonly(name?: string): Error {
 	return name
-		? new Error(`readonly property '${name} cannot be changed'`)
+		? new Error(`readonly property '${name}' cannot be changed`)
 		: new Error('readonly property cannot be changed');
 }
 
